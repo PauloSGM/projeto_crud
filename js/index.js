8d27c0635f2a864c8679c2476d0ec2fffa0e5f99
PauloSGM <meiotodo@hotmail.com>
2021-03-02 11:28:49 -0300

    Json

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
-let = dados [nome, sobrenome, dataDeNascimento, escolaridade];
+let dados = {
+nome : "Paulo",
+sobrenome : "Moura",
+dataNasc : "29/11/1986",
+escolaridade : "Superior",
+};
+
+
+console.log(pessoa);
+
+
+
+
 
 
