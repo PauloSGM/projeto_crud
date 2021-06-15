83afec1edcac422bc39ddf88513cb95a8a27e8ab
PauloSGM <meiotodo@hotmail.com>
2021-06-15 17:20:43 -0300

    entrando no PHP

--- js/index.js
+++ js/index.js
@@ -1,26 +1,3 @@
-let dados = [];
-
-let registro1 = {
-    nome: "Paulo",
-    sobrenome: "Moura",
-    dataNasc: "29/11/1986",
-    escolaridade: "Médio",
-};
-
-let registro2 = {
-    nome: "Fernanda",
-    sobrenome: "Gomes",
-    dataNasc: "25/08/1995",
-    escolaridade: "Superior",
-};
-dados = [registro1, registro2];
-
-function testar() {
-    if (Array.isArray(dados)) {
-
-       console.log("Certo!")
-    }
-}
 
 
 
