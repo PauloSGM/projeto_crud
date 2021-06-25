538b790723a2639d5809b17c0a1c4452676f0e4b
PauloSGM <meiotodo@hotmail.com>
2021-06-25 20:04:13 -0300

    escolhendo tabelas

--- index.php
+++ index.php
@@ -13,20 +13,43 @@
 
 <body>
 
-<?php 
+<?php switch($_POST['tabela']){
 
+case '1':
 $nome = $_POST[nome];
 $sobreNome = $_POST[sobreNome];
 $dataNasc = $_POST[dataNasc];
 $escolaridade = $_POST[escolaridade];
+break;
 
+case '2':
+    $nome2 = $_POST[nome];
+    $sobreNome2 = $_POST[sobreNome];
+    $dataNasc2 = $_POST[dataNasc];
+    $escolaridade2 = $_POST[escolaridade];
+    break;
 
+    case '3':
+        $nome3 = $_POST[nome];
+        $sobreNome3 = $_POST[sobreNome];
+        $dataNasc3 = $_POST[dataNasc];
+        $escolaridade3 = $_POST[escolaridade];
+        break;
+
+        case '4':
+            $nome4 = $_POST[nome];
+            $sobreNome4 = $_POST[sobreNome];
+            $dataNasc4 = $_POST[dataNasc];
+            $escolaridade4 = $_POST[escolaridade];
+            break;
+
+};
 ?>
     <div id="titulo" class="container-fluid col-md-12 ">
         <div class="row">
             <div class="col-md-12">
-            <h1>Meu Primeiro CRUD</h1>
-            <h3>CRUD básico com armazenamento em cookies</h3>
+            <h1>Sistema CRUD</h1>
+            <h3>Digite seus dados abaixo para salvá-los nas tabelas</h3>
         </div>
     </div>
     </div>
@@ -57,15 +80,23 @@ $escolaridade = $_POST[escolaridade];
                     <span class="input-group-text" id="inputGroup-sizing-sm">Digite sua escolaridade</span>
                 </div>
                 
-            </form>
+            
                 <input type="text" class="form-control" name="escolaridade" placeholder="Ex: nível médio" aria-label="Small" aria-describedby="inputGroup-sizing-sm">
-                
+</div>
                 <div class="container-fluid col-md-12">
+                <select name="tabela" class="form-select form-select-sm" aria-label=".form-select-sm example">
+  <option selected>Escolha em qual linha da tabela abaixo você quer salvar seus dados</option>
+  <option value="1">Tabela 1</option>
+  <option value="2">Tabela 2</option>
+  <option value="3">Tabela 3</option>
+  <option value="3">Tabela 4</option>
+</select>
     <div class="row">
+   
         <div class="col-col-md-12"><br>
             <button type="submit" class="btn btn-primary float-right">Adicionar informações</button>
         </div>
-    </div>
+    </div></form>
         </div>
     </div>
 </div>
@@ -91,6 +122,27 @@ $escolaridade = $_POST[escolaridade];
                     <td><?php echo $sobreNome?></td>
                     <td><?php echo $dataNasc?></td>
                     <td><?php echo $escolaridade?></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td><?php echo $nome2?></td>
+                    <td><?php echo $sobreNome2?></td>
+                    <td><?php echo $dataNasc2?></td>
+                    <td><?php echo $escolaridade2?></td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td><?php echo $nome3?></td>
+                    <td><?php echo $sobreNome3?></td>
+                    <td><?php echo $dataNasc3?></td>
+                    <td><?php echo $escolaridade3?></td>
+                </tr>
+                <tr>
+                    <th scope="row">4</th>
+                    <td><?php echo $nome4?></td>
+                    <td><?php echo $sobreNome4?></td>
+                    <td><?php echo $dataNasc4?></td>
+                    <td><?php echo $escolaridade4?></td>
                 </tr>
             </tbody>
         </table>
--- js/index.js
+++ js/index.js
@@ -1,10 +1,19 @@
+function Aluno(alunoNome, alunoNota) {
+    this.nome = alunoNome
+    this.nota = alunoNota
+};
 
+aluno1 = new Aluno("Paulo", 7)
+aluno2 = new Aluno("Roberto", 9)
+aluno3 = new Aluno("João", 5)
+aluno4 = new Aluno("Antônio", 4)
+aluno5 = new Aluno("Luiz", 6)
 
+function status(alunoASerAvaliado) {
+    notaQueOAlunoTirou = alunoASerAvaliado.nota;
+    if (notaQueOAlunoTirou >= 7)
+        console.log(`O aluno ${alunoASerAvaliado.nome} tirou a nota ${alunoASerAvaliado.nota}, portanto está aprovado`);
+        else console.log(`O aluno ${alunoASerAvaliado.nome} tirou a nota ${alunoASerAvaliado.nota} e infelizmente está reprovado`);
+};
 
-
-
-
-
-
-
-
+status(aluno3);
